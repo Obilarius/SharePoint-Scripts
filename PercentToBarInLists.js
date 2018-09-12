83489686b17147a0a072769906346fa2b2ab7c4b
Obilarius <git@walzenbach.com>
2018-09-12 11:51:41 +0200

    build in Gradiant Background

--- PercentToBarInLists.js
+++ PercentToBarInLists.js
@@ -12,11 +12,15 @@
         while (i < NodeSet.length){
             try {
                 CellContent = NodeSet[i].innerText || NodeSet[i].textContent;
-                if (HTMLregexp.test(CellContent)) { 
+                if (HTMLregexp.test(CellContent) && CellContent != "0%") { 
                     var childNode = NodeSet[i].getElementsByTagName("div")[0];
-                    childNode.style.backgroundColor = "lightskyblue";
+                    childNode.style.backgroundColor = "#EBEFF8";
                     childNode.style.width = CellContent;
                     childNode.style.float = "right"; 
+                    childNode.style.paddingBottom = "2px";
+                    childNode.style.paddingTop = "2px";
+                    childNode.style.paddingRight = "2px";
+                    childNode.style.filter = "progid:DXImageTransform.Microsoft.gradient( startColorstr='#EBEFF8', endColorstr='#DCE4F3')";
                 }
             } 
             catch(err){}
